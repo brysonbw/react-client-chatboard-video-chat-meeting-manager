c8bd71525525197f6175b630b2e6375e88d706ff
“bryson” <brysonward77@gmail.com>
2022-06-29 14:40:43 -0400

    Fixed typo in edit meeting form title modal

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -160,7 +160,7 @@ const Home = (props: Props) => {
                         </div>
                         <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                           <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                            Add Meeting
+                            Edit Meeting
                           </Dialog.Title>
                           <Formik
                     initialValues={{
